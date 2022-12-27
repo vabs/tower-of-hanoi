f7a2de786734c45f1f682b57314240712e314f8b
Vaibhav Somani <vaibhav@bloomfin.ca>
2022-12-26 20:09:32 -0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const NUM_DICS = 3;
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const [towers, setTowers] = useState([[3, 2, 1], [], []]);
+	const [selectedTowerIndex, setSelectedTowerIndex] = useState(undefined);
+
+	function onTowerSelected(clickedTowerIndex) {
+		console.log(selectedTowerIndex, clickedTowerIndex);
+		if(selectedTowerIndex !== undefined) {
+			const selectedTower = towers[selectedTowerIndex];
+			const clickedTower = towers[clickedTowerIndex];
+			if(selectedTower[0] > (clickedTower[0] ?? 999)) {
+				setSelectedTowerIndex(undefined);
+				return;
+			} else {
+				let newTowers = towers;
+				const poppedDisc = newTowers[selectedTowerIndex].shift();
+				newTowers[clickedTowerIndex].unshift(poppedDisc);
+				setTowers(newTowers);
+				if(newTowers[2].length === NUM_DICS) {
+					alert('you win!');
+				}
+			}
+			setSelectedTowerIndex(undefined);
+		} else {
+			setSelectedTowerIndex(clickedTowerIndex);
+		}
+	}
+
+	return (
+		<div className="App">
+			<h1>Tower of Hanoi</h1>
+			<div className="towers">
+				{towers.map((tower, towerIndex) => {
+					return (
+						<div className="tower" key={towerIndex} onClick={() => onTowerSelected(towerIndex)}>
+							<div className={"line " + (selectedTowerIndex === towerIndex ? "selected" : "")  } />
+								<div className="disks">
+									{tower.reverse().map((disk, diskIndex) => {
+										return (
+											<div
+												className="disk"
+												style={{
+													width: `${disk * 10 + 10}px`,
+												}}
+												key={diskIndex}
+											>
+												{disk}
+											</div>
+										);
+									})}
+								</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
